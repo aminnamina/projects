5f2dec8bebbf10d6ecdf7dd94d115589a4f3f7ea
Karina <kariyeva06@mail.ru>
2025-04-25 22:23:50 +0500

    Залила проект RK1

--- rk1/rk1/customers/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CustomerListCreateView, CustomerDetailView
-
-urlpatterns = [
-    path('', CustomerListCreateView.as_view(), name='customer-list'),
-    path('<int:pk>/', CustomerDetailView.as_view(), name='customer-detail'),
-]
--- rk1/rk1/reservations/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ReservationListCreateView, ReservationDetailView
-
-urlpatterns = [
-    path('', ReservationListCreateView.as_view(), name='reservation-list'),
-    path('<int:pk>/', ReservationDetailView.as_view(), name='reservation-detail'),
-]
